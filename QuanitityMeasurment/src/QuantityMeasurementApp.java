e111b3b6003d7f988a54647365747cb35d6571b4
Krrish-Kh <Krrish25k@gmail.com>
2026-05-04 15:51:19 +0530

    uc4

--- QuanitityMeasurment/src/QuantityMeasurementApp.java
+++ QuanitityMeasurment/src/QuantityMeasurementApp.java
@@ -1,8 +1,11 @@
 package com.apps.quantitymeasurement;
 public class QuantityMeasurementApp {
+
     enum LengthUnit {
         FEET(1.0),
-        INCH(1.0 / 12.0); // 1 inch = 1/12 feet
+        INCH(1.0 / 12.0),
+        YARD(3.0),
+        CM(0.393701 / 12.0);
 
         private final double toFeetFactor;
 
@@ -33,50 +36,51 @@ public class QuantityMeasurementApp {
 
         @Override
         public boolean equals(Object obj) {
-
             if (this == obj) return true;
             if (obj == null) return false;
             if (this.getClass() != obj.getClass()) return false;
 
             Length other = (Length) obj;
-
             return Double.compare(this.toFeet(), other.toFeet()) == 0;
         }
     }
 
-    public static boolean demonstrateLengthEquality(Length l1, Length l2) {
+    public static boolean compare(Length l1, Length l2) {
         return l1.equals(l2);
-    }
-
-    public static void demonstrateFeetEquality() {
-        Length f1 = new Length(1.0, LengthUnit.FEET);
-        Length f2 = new Length(1.0, LengthUnit.FEET);
-
-        System.out.println("Feet Equal (1.0, 1.0): " +
-                demonstrateLengthEquality(f1, f2));
-    }
-
-    public static void demonstrateInchesEquality() {
-        Length i1 = new Length(1.0, LengthUnit.INCH);
-        Length i2 = new Length(1.0, LengthUnit.INCH);
-
-        System.out.println("Inches Equal (1.0, 1.0): " +
-                demonstrateLengthEquality(i1, i2));
-    }
-
-    public static void demonstrateFeetInchesComparison() {
-        Length f = new Length(1.0, LengthUnit.FEET);
-        Length i = new Length(12.0, LengthUnit.INCH);
-
-        System.out.println("1 Feet == 12 Inches: " +
-                demonstrateLengthEquality(f, i));
     }
 
     public static void main(String[] args) {
 
-        demonstrateFeetEquality();
-        demonstrateInchesEquality();
-        demonstrateFeetInchesComparison();
+        System.out.println("1 Yard == 3 Feet: " +
+                compare(new Length(1.0, LengthUnit.YARD),
+                        new Length(3.0, LengthUnit.FEET)));
+
+        System.out.println("1 Yard == 36 Inches: " +
+                compare(new Length(1.0, LengthUnit.YARD),
+                        new Length(36.0, LengthUnit.INCH)));
+
+        System.out.println("2 Yard == 2 Yard: " +
+                compare(new Length(2.0, LengthUnit.YARD),
+                        new Length(2.0, LengthUnit.YARD)));
+
+        System.out.println("2 cm == 2 cm: " +
+                compare(new Length(2.0, LengthUnit.CM),
+                        new Length(2.0, LengthUnit.CM)));
+
+        System.out.println("1 cm == 0.393701 inch: " +
+                compare(new Length(1.0, LengthUnit.CM),
+                        new Length(0.393701, LengthUnit.INCH)));
+
+        System.out.println("1 cm != 1 feet: " +
+                compare(new Length(1.0, LengthUnit.CM),
+                        new Length(1.0, LengthUnit.FEET)));
+
+        Length yard = new Length(1.0, LengthUnit.YARD);
+        Length feet = new Length(3.0, LengthUnit.FEET);
+        Length inch = new Length(36.0, LengthUnit.INCH);
+
+        System.out.println("Transitive (yard == feet == inch): " +
+                (compare(yard, feet) && compare(feet, inch) && compare(yard, inch)));
 
         Length l = new Length(1.0, LengthUnit.FEET);
         System.out.println("Null check: " + l.equals(null));
